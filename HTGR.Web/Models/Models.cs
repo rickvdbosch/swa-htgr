0892f8198360eb1c220c9f7a6527f42ed0c42e72
Rick van den Bosch <rickvdbosch@outlook.com>
2022-07-21 10:03:12 +0200

    Initial commit

---
+++ HTGR.Web/Models/Models.cs
@@ -0,0 +1,23 @@
+﻿namespace HTGR.Web.Models
+{
+    public class SpeakerObject
+    {
+        public Collection collection { get; set; }
+    }
+
+    public class Collection
+    {
+        public Item[] items { get; set; }
+    }
+
+    public class Item
+    {
+        public Speaker[] data { get; set; }
+    }
+
+    public class Speaker
+    {
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+}
