227f26197bb03fd9aac4f3b36e1594811914bbe5
Rick van den Bosch <rickvdbosch@outlook.com>
2022-10-10 18:44:02 +0200

    Implemented API - part I

---
+++ HTGR.Functions/Entities/VideoEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+using Azure;
+using Azure.Data.Tables;
+
+namespace HTGR.Functions.Entities
+{
+    public class VideoEntity : ITableEntity
+    {
+        #region ITableEntity Members
+
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+        #endregion
+
+        [IgnoreDataMember]
+        public string Name
+        { 
+            get => RowKey;
+            set => RowKey = value;
+        }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Url { get; set; }
+    }
+}
---
+++ HTGR.Functions/GetVideos.cs
@@ -0,0 +1,25 @@
+using Azure.Data.Tables;
+
+using HTGR.Functions.Entities;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace HTGR.Functions
+{
+    public static class GetVideos
+    {
+        [FunctionName(nameof(GetVideos))]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "videos/{partitionKey}")] HttpRequest req,
+            [Table("Videos", Connection = "StorageConnectionString")] TableClient tableClient, string partitionKey, ILogger log)
+        {
+            var videos = tableClient.Query<VideoEntity>().Where(ve => ve.PartitionKey == partitionKey).ToList();
+            
+            return new OkObjectResult(videos);
+        }
+    }
+}
