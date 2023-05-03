2cf3a952a5d22deda4db1c24304dff980a78417a
Rick van den Bosch <rickvdbosch@outlook.com>
2023-05-03 17:57:07 +0200

    added /api/ prefix

--- HTGR.OtherAPI/Program.cs
+++ HTGR.OtherAPI/Program.cs
@@ -9,7 +9,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
-app.MapGet("/sessions", () =>
+app.MapGet("/api/sessions", () =>
 {
     return JsonSerializer.Deserialize<SessionModel[]>(File.ReadAllText($"{Environment.CurrentDirectory}/Data/daythree.json"));
 });
